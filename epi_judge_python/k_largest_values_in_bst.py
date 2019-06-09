650ed3ba7cbde933f528bd66aaf6e90c4168af2f
flai <franklai@protonmail.com>
2019-06-09 13:30:48 -0400

    14.3

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -3,9 +3,38 @@ from test_framework import generic_test, test_utils
 
 def find_k_largest_in_bst(tree, k):
     # TODO - you fill in here.
-    return None
+    # O(h+k) runtime, O(max(h, k)) space
+    # Description: Recurse to the bottom of the tree, when returning back up,
+    # record data until k nodes are recorded
+    def helper(tree):
+        if not tree or len(res) >= k:
+            return
+        helper(tree.right)
+        if len(res) < k:
+            res.append(tree.data)
+            helper(tree.left)
+    res = []
+    helper(tree)
+    return res
 
+# from collections import deque
+# def find_k_largest_in_bst_iter(tree, k):
+#     res = []
+#     cnt = 0
+#     stack = deque([tree])
 
+#     # stack [3] -> [5,3] -> [6,5,3]
+#     while cnt < k and stack:
+#         cur = stack[0]
+#         if cur.right:
+#             stack.append(cur.right)
+#             continue
+#         stack.pop()
+#         res.append(cur.data)
+#         cnt += 1
+#         if cur.left:
+#             stack.append(cur.left)
+#     return res
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
