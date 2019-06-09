e1ad6ff53c253811f2ff72b9e07fd535176df995
flai <franklai@protonmail.com>
2019-06-09 13:32:05 -0400

    14.2

--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -2,16 +2,34 @@ from test_framework import generic_test
 
 
 def find_first_greater_than_k(tree, k):
-    # TODO - you fill in here.
-    return None
+    ''' O(h) time, O(h) for stack space
+    '''
+    def helper(tree, k, candidate):
+        if not tree:
+            return candidate
+        if k < tree.data:
+            return helper(tree.left, k, tree)
+        return helper(tree.right, k, candidate)
+    return helper(tree, k, None)
 
 
 def find_first_greater_than_k_wrapper(tree, k):
     result = find_first_greater_than_k(tree, k)
     return result.data if result else -1
 
+def find_first_inorder_traversal(tree, k):
+    # TODO - you fill in here.
+    def helper(tree, k, candidate):
+        if not tree:
+            return candidate
+        if k <= tree.data:
+            return helper(tree.left, k, tree)
+        return helper(tree.right, k, candidate)
+    return helper(tree, k, None)
+
 
 if __name__ == '__main__':
+
     exit(
         generic_test.generic_test_main("search_first_greater_value_in_bst.py",
                                        'search_first_greater_value_in_bst.tsv',
