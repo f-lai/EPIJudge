9c01233d0c4f08a514b3fc6e11c0ee7ea4a69a3f
flai <franklai@protonmail.com>
2019-06-09 17:12:56 -0400

    9.1

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
-    return True
+    def compare_max_depths(tree, depth=0):
+        if not tree:
+            return (True, depth)
+        is_l_balanced, left_depth = compare_max_depths(tree.left, depth + 1)
+        if not is_l_balanced:
+            return (False, -1)
+        is_r_balanced, right_depth = compare_max_depths(tree.right, depth + 1)
+        balanced = is_l_balanced and is_r_balanced and abs(left_depth-right_depth) <= 1
+        return (balanced, max(left_depth, right_depth))
+    return compare_max_depths(tree)[0]
 
 
 if __name__ == '__main__':
