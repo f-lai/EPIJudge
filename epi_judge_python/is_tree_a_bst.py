05a3f617333f97f3cf014cadec2cba2ce0ff682c
flai <franklai@protonmail.com>
2019-06-09 13:29:56 -0400

    14.1

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
     # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+    if not low_range <= tree.data <= high_range:
+        return False
+    if tree.left:
+        is_left_bst = is_binary_tree_bst(tree.left, low_range=low_range, high_range=tree.data)
+    else:
+        is_left_bst = True
+    if tree.right:
+        is_right_bst = is_binary_tree_bst(tree.right, low_range=tree.data, high_range=high_range)
+    else:
+        is_right_bst = True
+    return is_left_bst and is_right_bst
 
 
 if __name__ == '__main__':
