3581dcef3f7e77b0ee43a72634c7d1e938c4c884
flai <franklai@protonmail.com>
2019-06-10 11:53:17 -0400

    Reimplement tree_from_preorder_inorder.py

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -2,7 +2,7 @@ from test_framework import generic_test
 from binary_tree_node import BinaryTreeNode as TN
 
 
-def binary_tree_from_preorder_inorder(preorder, inorder):
+def binary_tree_from_preorder_inorder_0(preorder, inorder):
     # TODO - you fill in here.
     if not preorder:
         if inorder:
@@ -37,8 +37,40 @@ def binary_tree_from_preorder_inorder(preorder, inorder):
     return tree
 
 
+def binary_tree_from_preorder_inorder(preorder, inorder):
+    def tree(pi, pj, ii, ij):
+        if pj <= pi or ij <= ii:
+            return None
+        root_inorder_idx = inorder_val_idx[preorder[pi]]
+        left_subtree_size = root_inorder_idx-ii
+        return TN(
+            preorder[pi],
+            tree(
+                pi+1,
+                pi+1+left_subtree_size,
+                ii,
+                root_inorder_idx,
+            ),
+            tree(
+                pi+1+left_subtree_size,
+                pj,
+                root_inorder_idx+1,
+                ij
+            )
+        )
+
+    if not preorder:
+        if inorder:
+            raise ValueError("Wrong inputs")
+        return None
+
+    inorder_val_idx = {val: idx for idx, val in enumerate(inorder)}
+
+    return tree(0, len(preorder), 0, len(preorder))
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("tree_from_preorder_inorder.py",
                                        'tree_from_preorder_inorder.tsv',
-                                       binary_tree_from_preorder_inorder))
+                                       binary_tree_from_preorder_inorder_0))
