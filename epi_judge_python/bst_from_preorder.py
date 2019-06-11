eb0b897ecdb1a7a201f60123537c638e3b42d05a
flai <franklai@protonmail.com>
2019-06-11 14:52:50 -0400

    Done chapter 14

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
-
+from binary_tree_node import BinaryTreeNode as BTN
 
 def rebuild_bst_from_preorder(preorder_sequence):
     # TODO - you fill in here.
-    return None
+    def bst(i, j):
+        if i == j:
+            return None
+        left_subtree_size = len(
+            [k
+             for k in range(i+1, j)
+             if preorder_sequence[k] < preorder_sequence[i]]
+        )
+        return BTN(
+            preorder_sequence[i],
+            bst(i+1, i+1+left_subtree_size),
+            bst(i+1+left_subtree_size, j)
+        )
+    return bst(0, len(preorder_sequence))
 
 
 if __name__ == '__main__':
--- epi_judge_python/bst_from_sorted_array.py
+++ epi_judge_python/bst_from_sorted_array.py
@@ -5,12 +5,17 @@ from test_framework.binary_tree_utils import (binary_tree_height,
                                               generate_inorder)
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
+from binary_tree_node import BinaryTreeNode as BTN
 
 
-def build_min_height_bst_from_sorted_array(A):
+def build_min_height_bst_from_sorted_array(arr):
     # TODO - you fill in here.
-    return None
-
+    def bst(i, j, arr):
+        if i >= j:
+            return None
+        m = (i+j)//2
+        return BTN(arr[m], bst(i, m, arr), bst(m+1, j, arr))
+    return bst(0, len(arr), arr)
 
 @enable_executor_hook
 def build_min_height_bst_from_sorted_array_wrapper(executor, A):
@@ -27,3 +32,6 @@ if __name__ == '__main__':
         generic_test.generic_test_main(
             "bst_from_sorted_array.py", 'bst_from_sorted_array.tsv',
             build_min_height_bst_from_sorted_array_wrapper))
+
+
+
